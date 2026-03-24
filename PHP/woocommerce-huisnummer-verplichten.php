f750cdd82f6ce5998f5c7fa522cd43d561f52402
egbertjan <egbertjan@scrolla.nl>
2026-03-24 15:08:39 +0100

    Create woocommerce-huisnummer-verplichten.php

---
+++ PHP/woocommerce-huisnummer-verplichten.php
@@ -0,0 +1,6 @@
+add_action( 'woocommerce_after_checkout_validation', 'validate_checkout', 10, 2);
+function validate_checkout( $data, $errors ){
+	if (  ! preg_match('/[0-9]/', $data[ 'billing_address_1' ] ) ){
+		$errors->add( 'address', 'Om je pakket te kunnen bezorgen, hebben we je huisnummer nodig. Vul dit alsjeblieft in bij het adres.' );
+	}
+}
