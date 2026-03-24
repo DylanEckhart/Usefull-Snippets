35bfe00456a8ee0ad18ef4adef137d2748237f89
egbertjan <egbertjan@scrolla.nl>
2026-03-24 15:11:05 +0100

    Create woocommerce-toon-laagste-prijs.php

---
+++ PHP/woocommerce-toon-laagste-prijs.php
@@ -0,0 +1,20 @@
+<?php
+// WooCommerce - Laagste prijs tonen van variabele producten
+add_filter( 'woocommerce_get_price_html', 'change_variable_products_price_display', 10, 2 );
+function change_variable_products_price_display( $price, $product ) {
+
+    if( ! $product->is_type('variable') ) return $price;
+
+    $prices = $product->get_variation_prices( true );
+
+    if ( empty( $prices['price'] ) )
+        return apply_filters( 'woocommerce_variable_empty_price_html', '', $product );
+
+    $min_price = current( $prices['price'] );
+    $max_price = end( $prices['price'] );
+    $prefix_html = '<span class="price-prefix">' . __('Vanaf: ') . '</span>';
+
+    $prefix = $min_price !== $max_price ? $prefix_html : '';
+
+    return apply_filters( 'woocommerce_variable_price_html', $prefix . wc_price( $min_price ) . $product->get_price_suffix(), $product );
+}
