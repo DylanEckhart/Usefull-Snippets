c5a35ab8ac09f5841faa577a5b51376bff77530b
egbertjan <egbertjan@scrolla.nl>
2026-03-24 15:10:35 +0100

    Update woocommerce-huisnummer-verplichten.php

--- PHP/woocommerce-huisnummer-verplichten.php
+++ PHP/woocommerce-huisnummer-verplichten.php
@@ -1,4 +1,5 @@
 <?php
+// WooCommerce - Huisnummer verplichten
 add_action( 'woocommerce_after_checkout_validation', 'validate_checkout', 10, 2);
 function validate_checkout( $data, $errors ){
 	if (  ! preg_match('/[0-9]/', $data[ 'billing_address_1' ] ) ){
