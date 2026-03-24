72e5b77bf7714b4ef937184f5ca4d71dfa7eb8da
egbertjan <egbertjan@scrolla.nl>
2026-03-24 15:12:36 +0100

    Update woocommerce-voorwaarden-new-tab.php

--- PHP/woocommerce-voorwaarden-new-tab.php
+++ PHP/woocommerce-voorwaarden-new-tab.php
@@ -1,5 +1,7 @@
 <?php
 // WooCommerce - Algemene voorwaarden openen in nieuw venster
+// Deze snippet verwijdert de standaard weergave van de algemene voorwaarden op de checkout, zodat deze in een nieuw venster geopend kunnen worden.
+
 function reialesa_woocommerce_checkout_terms_and_conditions() {
   remove_action( 'woocommerce_checkout_terms_and_conditions', 'wc_terms_and_conditions_page_content', 30 );
 }
