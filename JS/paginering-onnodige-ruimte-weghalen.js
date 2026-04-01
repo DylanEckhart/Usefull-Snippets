2112ba4fabe2c073dfed6cdee2e44187862469a7
egbertjan <egbertjan@scrolla.nl>
2026-04-01 11:04:25 +0200

    Create paginering-onnodige-ruimte-weghalen.js

---
+++ JS/paginering-onnodige-ruimte-weghalen.js
@@ -0,0 +1,24 @@
+// JetSmartFilters - Paginering onnodige ruimte weghalen
+
+// Deze code zorgt ervoor dat de paginering-widget van JetSmartFilters automatisch wordt verborgen als er geen pagina's zijn om weer te geven (bijvoorbeeld wanneer alle resultaten op één pagina passen).
+
+jQuery(document).ready(function($) {
+    function checkEmptyPagination() {
+        $('.jet-smart-filters-pagination').each(function() {
+            var $this = $(this);
+            var $widget = $this.closest('.elementor-widget-jet-smart-filters-pagination');
+
+            if ($this.find('a, span').length === 0) {
+                $widget.hide();
+            } else {
+                $widget.show();
+            }
+        });
+    }
+
+    checkEmptyPagination();
+
+    $(document).on('jet-filter-custom-content-render', function() {
+        checkEmptyPagination();
+    });
+});
