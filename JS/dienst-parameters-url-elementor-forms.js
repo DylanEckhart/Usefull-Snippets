a9673734a1f4f5e04b82737866d464a8fb5a299c
egbertjan <egbertjan@scrolla.nl>
2026-04-14 13:51:24 +0200

    Create dienst-parameters-url-elementor-forms.js

---
+++ JS/dienst-parameters-url-elementor-forms.js
@@ -0,0 +1,19 @@
+// Elementor - Dienst parameter aan URL
+
+// Deze snippet leest de 'dienst'-parameters uit de URL en vinkt bij het laden van de pagina automatisch de overeenkomende checkboxes in een formulier aan.
+
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has('dienst')) {
+        const dienstenString = urlParams.get('dienst');
+        const geselecteerdeDiensten = dienstenString.split('&');
+        const checkboxes = document.querySelectorAll('input[name="form_fields[diensten][]"]');
+
+        checkboxes.forEach(checkbox => {
+            if (geselecteerdeDiensten.includes(checkbox.value)) {
+                checkbox.checked = true;
+            }
+        });
+    }
+});
